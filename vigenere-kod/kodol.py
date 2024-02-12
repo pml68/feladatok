71d321601cc37222c883bb04390edb43fef468c2
pml68 <tutorialmester@gmail.com>
2024-02-12 18:48:00 +0100

    feat: remove unused variables

--- vigenere-kod/kodol.py
+++ vigenere-kod/kodol.py
@@ -6,7 +6,7 @@ tabla_sorok = []
 with open("vtabla.txt", "r", encoding="utf-8") as f:
     tabla_sorok = [[*line.strip()] for line in f.readlines()]
 
-a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z = zip(*tabla_sorok)
+a,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_ = zip(*tabla_sorok)
 
 print("1. feladat:")
 
